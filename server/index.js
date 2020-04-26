233ab0e98a42613c451291946c8acc990058fd07
presdk <dawinkweon@gmail.com>
2020-04-26 13:45:32 +1200

    Add swagger for REST API documentation and forward default req to the docs page

--- server/index.js
+++ server/index.js
@@ -1,11 +1,35 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express');
+const express = require("express");
 const app = express();
+
+// Set up swagger for api documentation
+const expressSwagger = require("express-swagger-generator")(app);
+let options = {
+  swaggerDefinition: {
+    info: {
+      description: "Server for flat-app",
+      title: "API for flat-app",
+      version: "1.0.0",
+    },
+    host: "localhost:3000",
+    basePath: "/",
+    produces: ["application/json"],
+    schemes: ["http"],
+  },
+  basedir: __dirname,
+  files: ["./routes/**/*.js"],
+};
+
+expressSwagger(options);
+
+app.get("/", (req, res) => {
+  // Default forward to swagger docs page
+  req.url = "/api-docs/";
+  app.handle(req, res);
+});
+
 const port = process.env.PORT || 4000;
-
-app.get('/', (req, res) => res.send('Hello World'));
-
 app.listen(port, () => console.log(`App listening on port: ${port}`));
