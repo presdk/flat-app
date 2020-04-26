f90801146c5e6c61a37bbdbdf887d82273c503f0
presdk <dawinkweon@gmail.com>
2020-04-26 14:38:02 +1200

    Add mongo db adapter and connect to db Add users route Add README for setting up server env

--- server/index.js
+++ server/index.js
@@ -1,35 +1,65 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-const express = require("express");
-const app = express();
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
+const MONGODB_DB = process.env.MONGODB_DB || "flat-app";
+const PORT = process.env.PORT || 4000;
 
-// Set up swagger for api documentation
-const expressSwagger = require("express-swagger-generator")(app);
-let options = {
-  swaggerDefinition: {
-    info: {
-      description: "Server for flat-app",
-      title: "API for flat-app",
-      version: "1.0.0",
+main();
+
+async function main() {
+  const mongoClient = await MongoClient.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+  });
+
+  const db = mongoClient.db(MONGODB_DB);
+
+  const app = express();
+  // Attach mongo db
+  app.use(function attachDb(req, res, next) {
+    req.db = db;
+    next();
+  });
+
+  // Set up swagger for api documentation
+  const expressSwagger = require("express-swagger-generator")(app);
+  let options = {
+    swaggerDefinition: {
+      info: {
+        description: "Server for flat-app",
+        title: "API for flat-app",
+        version: "1.0.0",
+      },
+      host: "localhost:3000",
+      basePath: "/",
+      produces: ["application/json"],
+      schemes: ["http"],
     },
-    host: "localhost:3000",
-    basePath: "/",
-    produces: ["application/json"],
-    schemes: ["http"],
-  },
-  basedir: __dirname,
-  files: ["./routes/**/*.js"],
-};
+    basedir: __dirname,
+    files: ["./routes/**/*.js"],
+  };
 
-expressSwagger(options);
+  expressSwagger(options);
 
-app.get("/", (req, res) => {
   // Default forward to swagger docs page
-  req.url = "/api-docs/";
-  app.handle(req, res);
-});
+  app.get("/", (req, res) => {
+    req.url = "/api-docs/";
+    app.handle(req, res);
+  });
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`App listening on port: ${port}`));
+  // Users route
+  const users = require("./routes/users");
+  app.use("/users", users);
+
+  app.listen(PORT, (err) => {
+    if (err) {
+      throw err;
+    }
+    // eslint-disable-next-line no-console
+    console.log(`api-server listening on port ${PORT}`);
+  });
+}
