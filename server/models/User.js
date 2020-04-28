f9c767927ab293482f67fc1ff4a537a88fd63aa4
presdk <dawinkweon@gmail.com>
2020-04-28 20:03:23 +1200

    improv(users model): add user type to tell apart from admin and user refactor(users route): tidy up the promises error handling (because I know how to do it now hehe)

--- server/models/User.js
+++ server/models/User.js
@@ -2,15 +2,26 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const UserType = Object.freeze({
+  User: "user",
+  Admin: "admin",
+});
+
 /**
  * @typedef User
  * @property {string} name.required - The name of the user
  * @property {string} email_address.required - The email address
+ * @property {string} type - The type of user: `user` or `admin`
  * @property {Boolean} is_deleted - True if the user has been deleted
  */
 const UserSchema = new Schema({
   name: { type: String, required: true },
   email_address: { type: String, required: true },
+  type: {
+    type: String,
+    default: UserType.User,
+    enum: Object.values(UserType),
+  },
   is_deleted: { type: Boolean, default: false },
 });
 
@@ -23,5 +34,6 @@ const User = mongoose.model("User", UserSchema);
 
 module.exports = {
   User: User,
+  UserType: UserType,
   UserSchema: UserSchema,
 };
