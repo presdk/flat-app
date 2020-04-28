2e38f48296a8f6cdc9b7dbf3a856ca9c9cb13507
94lite <sd.94lite@gmail.com>
2020-04-28 20:49:55 +1200

    Add select option for logging in

--- client/src/PageContainers/Login/index.jsx
+++ client/src/PageContainers/Login/index.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
+import axios from 'axios';
+import { Select, MenuItem } from '@material-ui/core';
 
 class PageLogin extends React.Component {
     constructor(props) {
         super(props);
         this.state={
-
+            user_options: [],
+            selected_user: undefined
         }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/users').then(res => {
+            this.setState({ user_options: res.data });
+        });
     }
     
     render() {
+        const select_items = this.state.user_options.map(user => {
+            return (
+                <MenuItem 
+                    key={user._id}
+                    value={user.name}
+                >
+                    {user.name}
+                </MenuItem>
+            )
+        })
         return (
-            <div>Login to the app as a registered user here</div>
+            <div>
+                <Select
+                    value={this.state.selected_user}
+                    onChange={(event) => this.setState({ selected_user: event.target.value })}
+                    displayEmpty
+                >
+                    {select_items}
+                </Select>
+            </div>
         )
     }
 }
