831257ff08e63defcabadd7559d364127c01bda2
presdk <dawinkweon@gmail.com>
2020-05-02 15:24:41 +1200

    feature(init): initialize repo for the .net core tool to process emails into bill rest api resources

---
+++ Bills/Bills/BillsRunner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bills
+{
+    /// <summary>
+    /// The runner class that fetches and processes emails to generate bills via REST APIs.
+    /// </summary>
+    public class BillsRunner
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Bills runner is running...");
+        }
+    }
+}
