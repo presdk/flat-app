9b02f8e17608941bd19954685bd11be4a1173d25
presdk <windarena@gmail.com>
2020-09-19 19:33:44 +1200

    improv(style): add roboto font
    * add styled components

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,21 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import store from './redux/store';
 import PageRouter from './Routes';
 import Header from './Components/header';
+import styled from "styled-components";
+
+const GlobalStyle = styled.div`
+  * {
+    font-family: 'Roboto', sans-serif
+  }
+`;
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <Header />
-      <PageRouter />
+      <GlobalStyle>
+        <Header />
+        <PageRouter />
+      </GlobalStyle>
     </Router>
   </Provider>,
   document.getElementById('root')
